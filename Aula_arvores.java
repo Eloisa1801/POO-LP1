ffc2e3bd8575b32aec5a500b497803bea1696ab0
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2022-11-16 21:13:34 -0400

    Tree

---
+++ Aula_arvores.java
@@ -0,0 +1,13 @@
+package LP1.arvores;
+
+public class Aula_arvores {
+      //criar a seguinte arvore e imprimir conforme os três percursos apresentados       
+      
+      // pré ordem - trata a raiz, percorre esquerda sae, percorre a direita sad (RAIZ; <-; ->):   + * + 3 6 - 4 1 5
+
+      //simétrica - percorre sea, trata a raiz, percorre a sad (<-; RAIZ; ->): 3 + 6 * 4 - 1 + 5
+
+      // pos ordem - percorre sae, percorre sad, trata raiz (<-; ->; RAIZ): 3 6 + 4 1 - * 5 + 
+
+
+}
---
+++ Node.java
@@ -0,0 +1,13 @@
+package LP1.arvores;
+
+public class Node {
+    int info;
+    Node esq;
+    Node dir;
+
+  public Node(int info){
+    this.info = info;
+    this.esq = null;
+    this.dir = null;
+  }
+}
